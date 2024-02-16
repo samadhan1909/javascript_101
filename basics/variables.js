a99a6bb3d88e671514c011ba48a3f68838bade86
samadhan1909 <155223318+samadhan1909@users.noreply.github.com>
2024-02-16 17:58:58 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "samadhan@gmail.com"
+var accountPassword = "12345"
+accountCity ="Pune"
+
+// accountId = 2 //not allowed
+accountEmail="hc@hc.com"
+accountPassword= "54321"
+accountCity= "Mumbai"
+let accountState;
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/* 
+prefer not to use var for variable 
+Because there is a problem in block scope of Java language 
+*/
