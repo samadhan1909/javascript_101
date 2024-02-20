49323299cd0009157eb8cca9c5bcf04072824201
samadhan1909 <samadhansurwase30@gmail.com>
2024-02-20 19:01:08 +0530

    added conversionVaariables file

--- basics/03-conversionVariables.js
+++ basics/03-conversionVariables.js
@@ -1,2 +1,26 @@
-console.log("samadhan")
-let score = 12
+let score = null
+
+// console.log(typeof score);
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+// "33" => 33 
+// "33abc" => NaN
+// true => 1; false =>0
+
+let isLoggedIn = 0
+
+let booleanisLoggedIn = Boolean (isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+// 1 => true ; 0 => false
+// null => false 
+// "samadhan" => true ; "" => false 
+
+let someNumber = 33 
+
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
