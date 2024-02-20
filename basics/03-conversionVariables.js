afd13a82cc637ed884da4264caf7a149bdfb2047
samadhan1909 <samadhansurwase30@gmail.com>
2024-02-20 19:52:36 +0530

    added operations to conversionVariables file

--- basics/03-conversionVariables.js
+++ basics/03-conversionVariables.js
@@ -22,5 +22,39 @@ let booleanisLoggedIn = Boolean (isLoggedIn)
 let someNumber = 33 
 
 let stringNumber = String (someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************** Operations ******************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2); 
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello "
+let str2 = "world"
+let str3 = str1+ str2
+
+// console.log(str3);
+// console.log("1"+2);
+// console.log( 1+ 1+"2");
+// console.log("1"+2+2);
+
+// console.log((3+2)*5 % 4); // use parethesis () for tricky opearations
+
+console.log( true);
+console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2 
+
+let gameCounter= 100
+gameCounter++;
+console.log(gameCounter);
