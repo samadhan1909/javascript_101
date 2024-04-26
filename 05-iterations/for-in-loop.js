ba56215d510b3067aac5adc1716ab61098adc5d7
samadhan1909 <samadhansurwase30@gmail.com>
2024-04-26 20:42:31 +0530

    high order loop file added

---
+++ 05-iterations/for-in-loop.js
@@ -0,0 +1,16 @@
+// const myObj= {
+//     cpp: "C++",
+//     py: "python",
+//     rb: "Ruby",
+//     swift: "Swift by Apple"
+// }
+
+// for (const key in myObj) {
+//    console.log(`${key} shrtcut is for ${myObj[key]}`);
+// }
+
+const programming = ["js", "rb", "cpp", "java", "py"]
+
+for (const key in programming) {
+    console.log(programming[key]);    
+}
---
+++ 05-iterations/tempCodeRunnerFile.js
@@ -0,0 +1,10 @@
+const myObj= {
+//     cpp: "C++",
+//     py: "python",
+//     rb: "Ruby",
+//     swift: "Swift by Apple"
+// }
+
+// for (const key in myObj) {
+//    console.log(`${key} shrtcut is for ${myObj[key]}`);
+// }
