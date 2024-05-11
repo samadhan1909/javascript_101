6aba9c7227fed1f387a0ebd6fa93c696459ec1d8
samadhan1909 <samadhansurwase30@gmail.com>
2024-05-11 13:31:50 +0530

    map filter file

---
+++ 05-iterations/06-map-filter=reduce.js
@@ -0,0 +1,22 @@
+// const coding = ["js", "ruby", "java", "python", "cpp"]
+
+// const values = coding.forEach(item => {
+//     console.log(item);
+//     // return item                  // foreach loop doesnt give any return value
+// });
+
+// console.log(values);
+
+myNum = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNum = myNum.filter((num) => num > 4)
+// const newNum = myNum.filter((num) => {
+//     return num > 4
+// })
+
+newNum = []
+
+myNum.forEach((num) => {
+    
+});
+console.log(newNum);
